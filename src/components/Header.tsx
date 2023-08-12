034eb0a2138bf9381e34b5457cba5c6a98c0e024
ritesharma1819 <riteshsharmaps_me18@its.edu.in>
2023-08-12 15:09:46 +0530

    feat(ui):- made ui of header

---
+++ src/components/Header.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import MenuIcon from "@/public/assets/icons/menuIcon.svg";
+import CloseIcon from "@/public/assets/icons/closeIcon.svg";
+import FacebookIcon from "@/public/assets/icons/facebookIcon.svg";
+import LinkedInIcon from "@/public/assets/icons/linkedInIcon.svg";
+import TwitterIcon from "@/public/assets/icons/twiterIcon.jpg";
+
+import logo from "@/public/assets/profile.jpg";
+import { navLinksdata } from "@/src/utils/constants";
+import Image from "next/image";
+
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
+      <div>
+        {/* make a circle with your thumb and index finger and place it on the
+        screen */}
+        <Image className="w-32" src={logo} alt="logo" />{" "}
+      </div>
+      <div>
+        <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
+          {navLinksdata.map(({ _id, title, link }) => (
+            <li
+              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+              key={_id}
+            >
+              <Link
+                activeClass="active"
+                to={link}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <span
+          onClick={() => setShowMenu(!showMenu)}
+          className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </span>
+        {showMenu && (
+          <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
+            <div className="flex flex-col gap-8 py-2 relative">
+              <div>
+                <Image className="w-32" src={logo} alt="logo" />
+                <p className="text-sm text-gray-400 mt-2">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Earum soluta perspiciatis molestias enim cum repellat, magnam
+                  exercitationem distinctio aliquid nam.
+                </p>
+              </div>
+              <ul className="flex flex-col gap-4">
+                {navLinksdata.map((item) => (
+                  <li
+                    key={item._id}
+                    className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+                  >
+                    <Link
+                      onClick={() => setShowMenu(false)}
+                      activeClass="active"
+                      to={item.link}
+                      spy={true}
+                      smooth={true}
+                      offset={-70}
+                      duration={500}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex flex-col gap-4">
+                <h2 className="text-base uppercase font-titleFont mb-4">
+                  Find me in
+                </h2>
+                <div className="flex gap-4">
+                  <span className="bannerIcon">
+                    <FacebookIcon />
+                  </span>
+                  <span className="bannerIcon">
+                    <Image
+                      src={TwitterIcon}
+                      alt="twitter"
+                      width={20}
+                      height={20}
+                    />
+                  </span>
+                  <span className="bannerIcon">
+                    <LinkedInIcon />
+                  </span>
+                </div>
+              </div>
+              <span
+                onClick={() => setShowMenu(false)}
+                className="absolute top-4 right-4 text-gray-400 hover:text-designColor duration-300 text-2xl cursor-pointer"
+              >
+                <CloseIcon />
+              </span>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+export const navLinksdata = [
+  {
+    _id: 1001,
+    title: "Home",
+    link: "home",
+  },
+  {
+    _id: 1002,
+    title: "About Me",
+    link: "about-me",
+  },
+  {
+    _id: 1003,
+    title: "Projects",
+    link: "projects",
+  },
+
+  {
+    _id: 1004,
+    title: "Contact",
+    link: "contact",
+  },
+];
