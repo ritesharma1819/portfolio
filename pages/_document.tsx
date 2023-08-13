bf9d9538e3fb1d1fee0c416a9df264ace38b990b
ritesharma1819 <riteshsharmaps_me18@its.edu.in>
2023-08-13 19:58:41 +0530

    fix(seo):- fix seo perfomance

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,29 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Ritesh Sharma</title>
+        <meta name="title" content="Ritesh Sharma" />
+        <meta name="description" content="My Personal Corner on the Internet" />
+        <meta name="theme-color" content="#fff" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Ritesh Sharma" />
+        <meta
+          property="og:description"
+          content="My Personal Corner on the Internet"
+        />
+        <meta property="og:image" content="https://example.com/hritik" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Ritesh Sharma" />
+        <meta
+          property="twitter:description"
+          content="My Personal Corner on the Internet"
+        />
+        <meta property="twitter:image" content="https://example.com/hritik" />
+      </Head>
       <body>
         <Main />
         <NextScript />
